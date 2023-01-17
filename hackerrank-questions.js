7e6d9f879579cb5b42d955b5b69c85612d091269
James Hudson <jmh357@comcast.net>
2023-01-17 13:34:42 -0600

    sockMerchant solution 2

--- hackerrank-questions.js
+++ hackerrank-questions.js
@@ -1,3 +1,5 @@
+
+// WRONG ANSWER (according to hackerrank -- although it does produce the correct answer...)
 function sockMerchant(n, ar) {
   let pairs = 0
   for (let i = 0; i < ar.length; i++) {
@@ -17,4 +19,27 @@ function sockMerchant(n, ar) {
 }
 
 // console.log(sockMerchant(9, [10,20,20,10,10,30,50,10,20]))
-console.log(sockMerchant(10,  [1,2,3,2,4,3,1,2,3,4]))
+// console.log(sockMerchant(10,  [1,2,3,2,4,3,1,2,3,4]))
+
+// ________________________________________
+// ________________________________________
+
+
+// CORRECT ANSWER (according to hackerrank)
+function sockMerchant2(n, ar) {
+  let socks = {}
+  let pairs = 0
+
+  for(let element of ar) {
+    socks[element] = socks[element] + 1 || 1
+    console.log('socks obj: ', socks)
+    if(socks[element] % 2 === 0) {
+      pairs ++
+      console.log('PAIRS: ', pairs)
+    }
+  }
+  return pairs
+}
+
+
+console.log(sockMerchant2(9, [10,20,20,10,10,30,50,10,20]))
